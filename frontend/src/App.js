a0419672efcc7fd757188f6a83a6aad8965c7f0a
bratside5 <bratside5@gmail.com>
2020-05-25 20:02:53 +0200

    added twin macro and menu screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./App.css";
+import YouTubeLogo from "./assets/youtube-3.svg";
+import SpotifyLogo from "./assets/spotify-2.svg";
+import NetflixLogo from "./assets/netflix_logo.svg";
+import PrimeLogo from "./assets/prime-video-logo-2020.svg";
+import "tailwindcss/dist/base.css";
+import "twin.macro";
+import {
+  MainWrapper,
+  MenuContainer,
+  GridWrapper,
+  MenuImage,
+} from "./components/UI_styled-components/UI";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <MainWrapper>
+        <GridWrapper>
+          <MenuContainer
+            href="https://m.youtube.com/watch?v=8WyGaus8ax0&list=RDMM8WyGaus8ax0&start_radio=1"
+            target="_blank"
+          >
+            <MenuImage src={YouTubeLogo} />
+          </MenuContainer>
+          <MenuContainer href="https://open.spotify.com/" target="_blank">
+            <MenuImage src={SpotifyLogo} />
+          </MenuContainer>
+          <MenuContainer href="https://netflix.com" target="_blank">
+            <MenuImage src={NetflixLogo} />
+          </MenuContainer>
+          <MenuContainer
+            href="https://www.primevideo.com/region/eu/storefront/home/ref=atv_nb_sf_hm"
+            target="_blank"
+          >
+            <MenuImage src={PrimeLogo} />
+          </MenuContainer>
+        </GridWrapper>
+      </MainWrapper>
+    </>
   );
 }
 
---
+++ frontend/src/components/UI_styled-components/UI.js
@@ -0,0 +1,8 @@
+import tw from "twin.macro";
+
+export const MainWrapper = tw.div`flex items-center justify-center align-middle content-start h-screen w-screen bg-gray-800`;
+export const GridWrapper = tw.div`grid grid-cols-4 gap-4 items-center p-3`;
+export const MenuContainer = tw.a`flex items-center justify-center h-48 max-h-full bg-gray-700 align-middle content-start p-6 shadow-lg rounded-lg`;
+
+export const MenuImage = tw.img`object-contain object-cover object-scale-down h-48`;
+export const MenuTitle = tw.div``;
