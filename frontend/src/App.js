ad0b9ad8efa71ccd453547edb525604b92c32534
bratside5 <bratside5@gmail.com>
2020-05-27 18:34:34 +0200

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Spotify from "./components/Channels/Spotify/Spotify";
 import Netflix from "./components/Channels/Netflix/Netflix";
 import Prime from "./components/Channels/Prime/Prime";
 import RetroPie from "./components/Channels/Retropie/RetroPie";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
