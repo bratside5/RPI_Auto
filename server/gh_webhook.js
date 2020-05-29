7def4a3ad39538d06f0ac376446d40463da4b7eb
bratside5 <bratside5@gmail.com>
2020-05-29 11:44:30 +0200

    wh test

--- server/gh_webhook.js
+++ server/gh_webhook.js
@@ -21,16 +21,16 @@ app.post("/payload", function (req, res) {
   console.log("pulling code from GitHub...");
 
   // reset any changes that have been made locally
-  exec("git -C ~  reset --hard", execCallback);
+  exec("cd ~/home/pi/RPI_Auto && git -C   reset --hard", execCallback);
 
   // and ditch any files that have been added locally too
-  exec("git -C ~ clean -df", execCallback);
+  exec("cd ~/home/pi/RPI_Auto && git -C  clean -df", execCallback);
 
   // now pull down the latest
-  exec("git -C ~ pull -f", execCallback);
+  exec("cd ~/home/pi/RPI_Auto && git -C  pull -f", execCallback);
 
   // and npm install with --production
-  exec("npm -C ~ install --production", execCallback);
+  exec("cd ~/home/pi/RPI_Auto && npm -C  install --production", execCallback);
 });
 
 app.listen(6000, function () {
